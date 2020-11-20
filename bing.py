6c022d95ed16673f836a6ed804bc7e352149ecc7
Siefe <66122762+Siefe@users.noreply.github.com>
2020-11-20 17:31:00 +0800

    Update bing.py

--- bing.py
+++ bing.py
@@ -15,7 +15,7 @@ date=getmidstring(imgurl.text,'startdate":"','","')
 filename=date+".jpg"
 
 img=requests.get(url)
-open("src/"+filename, 'wb').write(img.content)
+open("pic/"+filename, 'wb').write(img.content)
 
-os.system("git add src/"+filename)
+os.system("git add pic/"+filename)
 os.system("git commit -m '"+date+"的必应壁纸'")
